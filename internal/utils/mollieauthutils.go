e322c0aba4c1dc58f8f56fc603a2b5e033980419
André Oliveira <andre.oliveira@mollie.com>
2026-03-31 14:02:50 +0100

    Add Security Scheme Check

--- internal/utils/mollieauthutils.go
+++ internal/utils/mollieauthutils.go
@@ -40,7 +40,7 @@ func extractSecurityToken(val interface{}) *string {
 		}
 		v = v.Elem()
 	}
-	for _, fieldName := range []string{"APIKey", "OAuth"} {
+	for _, fieldName := range []string{"APIKey", "OrganizationAccessToken", "OAuth"} {
 		field := v.FieldByName(fieldName)
 		if field.IsValid() && field.Kind() == reflect.Ptr && !field.IsNil() {
 			s := field.Elem().String()
